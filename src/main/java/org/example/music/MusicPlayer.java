3603f5efff7532069df61b7e0fd725a0f9831d60
Yurii_Moroziuk <yurii_moroziuk@epam.com>
2023-04-20 19:13:53 +0300

    Dependency Injection (Part 2)

--- src/main/java/org/example/music/MusicPlayer.java
+++ src/main/java/org/example/music/MusicPlayer.java
@@ -1,14 +1,53 @@
 package org.example.music;
 
+import java.util.List;
+
 public class MusicPlayer {
+    private List<Music> musicList;
     private Music music;
+    private String name;
+    private int volume;
 
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+    public MusicPlayer() {
     }
+
+    public void playMusic() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
 }
--- src/main/java/org/example/music/TestMusic.java
+++ src/main/java/org/example/music/TestMusic.java
@@ -1,6 +1,5 @@
 package org.example.music;
 
-import org.example.TestBean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMusic {
@@ -10,6 +9,7 @@ public class TestMusic {
         //Music music = context.getBean("musicBean", Music.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println("Name: " + musicPlayer.getName() + " Volume: " + musicPlayer.getVolume());
         context.close();
     }
 }
