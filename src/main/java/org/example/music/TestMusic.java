4b6a13613df8c55e19b9a16d828a337490082849
Yurii_Moroziuk <yurii_moroziuk@epam.com>
2023-04-21 14:56:53 +0300

    Scope Bean

--- src/main/java/org/example/music/TestMusic.java
+++ src/main/java/org/example/music/TestMusic.java
@@ -7,9 +7,16 @@ public class TestMusic {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
         //Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println("Name: " + musicPlayer.getName() + " Volume: " + musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        secondMusicPlayer.setVolume(66);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+        firstMusicPlayer.playMusic();
+        System.out.println("Name: " + firstMusicPlayer.getName() + " Volume: " + firstMusicPlayer.getVolume());
         context.close();
     }
 }
